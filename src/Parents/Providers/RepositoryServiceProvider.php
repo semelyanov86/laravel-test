60041e40089d4f28f9f7aef0a75f40f1f7650d64
Sergey <sergey86@gmail.com>
2022-08-31 07:32:46 +0200

    Register FacadeInterface

--- src/Parents/Providers/RepositoryServiceProvider.php
+++ src/Parents/Providers/RepositoryServiceProvider.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Parents\Providers;
 
+use Domains\Category\Facades\CategoryFacade;
+use Domains\Category\Facades\CategoryFacadeInterface;
 use Domains\Category\Repositories\CategoryRepositoryInterface;
 use Domains\Category\Repositories\Eloquent\CategoryRepository;
+use Domains\Product\Facades\ProductFacade;
+use Domains\Product\Facades\ProductFacadeInterface;
 use Domains\Product\Repositories\Eloquent\ProductRepository;
 use Domains\Product\Repositories\ProductRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
@@ -16,5 +20,7 @@ class RepositoryServiceProvider extends ServiceProvider
     {
         $this->app->bind(ProductRepositoryInterface::class, ProductRepository::class);
         $this->app->bind(CategoryRepositoryInterface::class, CategoryRepository::class);
+        $this->app->bind(CategoryFacadeInterface::class, CategoryFacade::class);
+        $this->app->bind(ProductFacadeInterface::class, ProductFacade::class);
     }
 }
